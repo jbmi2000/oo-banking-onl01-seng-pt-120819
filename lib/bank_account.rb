06133092c219635f642fe3ac834f5fe576b26941
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-29 22:16:32 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
 
+attr_reader :name
+attr_accessor :balance, :status, :bank_account
+
+def initialize(name, balance = 1000)
+  @name = name
+  @balance = balance
+  @status = "open"
+end
+
+
+
 end
