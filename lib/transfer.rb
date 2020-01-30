a04fab481c72f3bf07be6089b5f592b3968328ca
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-30 04:30:16 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
+    if valid? && sender.balance >= amount && status != "complete"
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
 
