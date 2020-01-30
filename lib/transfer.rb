d1a893ecdc91ede4b8320bfe0617e948d9afaf5b
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-30 03:01:50 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,20 @@
 class Transfer
 
-  attr_accessor :transfer_amount, :sender, :receiver, :status
+  # attr_accessor :transfer_amount, :sender, :receiver, :status
+  # 
+  # def initialize(transfer, sender, receiver)
+  #   @transfer = transfer
+  #   @transfer_amount = transfer_amount
+  #   @status = "pending"
+  #   @sender = sender
+  # end
+  attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize(transfer, sender, receiver)
-    @transfer = transfer
-    @transfer_amount = transfer_amount
-    @status = "pending"
-    @sender = sender
-  end
+ def initialize(sender, receiver, amount)
+   @sender = sender
+   @receiver = receiver
+   @amount = amount
+   @status = "pending"
+ end
 
 end
