cf585991910f8b766a3a8331c843df74c859416a
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-30 04:52:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,19 +15,18 @@ class Transfer
   end
 
   def execute_transaction
-
   if valid? && @sender.balance >= @amount && @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
 
       @status = "complete"
-
+  
     else
       @status = "rejected"
-
+      "Transaction rejected.  Please check your account balance."
+  
     end
   end
-
 
   def reverse_transfer
     if @status == "complete"
