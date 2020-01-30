9e6749b334227b4893a6eecd8b1496b6852c5f7a
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-30 04:52:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
 
     else
       @status = "rejected"
-      return "Transaction rejected.  Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
 
     end
   end
